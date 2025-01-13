4c48b1fe163db15d5f47ca9e4622c0f9e2b06fe2
GastonMora42 <gaston-mora@hotmail.com>
2025-01-13 13:46:23 -0300

    Cambios en config next para el deploy

--- src/components/auth/login-form.tsx
+++ src/components/auth/login-form.tsx
@@ -166,17 +166,17 @@ const LoginForm: React.FC = () => {
           <div className="mt-6 grid grid-cols-3 gap-3">
             <div>
               <a href="#" className="w-full flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-                <img className="h-5 w-5" src="https://www.svgrepo.com/show/512120/facebook-176.svg" alt="Facebook" />
+              <Image className="h-5 w-5" src="https://www.svgrepo.com/show/512120/facebook-176.svg" alt="Facebook" />
               </a>
             </div>
             <div>
               <a href="#" className="w-full flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-                <img className="h-5 w-5" src="https://www.svgrepo.com/show/513008/twitter-154.svg" alt="Twitter" />
+              <Image className="h-5 w-5" src="https://www.svgrepo.com/show/513008/twitter-154.svg" alt="Twitter" />
               </a>
             </div>
             <div>
               <a href="#" className="w-full flex items-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50">
-                <img className="h-5 w-5" src="https://www.svgrepo.com/show/506498/google.svg" alt="Google" />
+              <Image className="h-5 w-5" src="https://www.svgrepo.com/show/506498/google.svg" alt="Google" />
               </a>
             </div>
           </div>
