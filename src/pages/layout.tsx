26279577d9126a4a14da7afc3598ea4b06220121
GastonMora42 <gaston-mora@hotmail.com>
2025-01-13 15:09:16 -0300

    Cambios en config next para el deploy

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,25 +1,22 @@
-import type { Metadata } from "next";
-import "./globals.css";
+// src/pages/layout.tsx
 import { inter } from "@/components/fonts";
 import ConfigureAmplifyClientSide from "@/amplify-cognito-config";
 
-export const metadata: Metadata = {
-  title: "Next.js Cognito Authentication",
-  description: "Cognito authenticated Next.js app.",
-};
-
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
-      <body className={`${inter.className} antialiased`}>{children}</body>
-      <>
-      <ConfigureAmplifyClientSide />
-      {children}
-      </>
+      <head>
+        <title>Next.js Cognito Authentication</title>
+        <meta name="description" content="Cognito authenticated Next.js app." />
+      </head>
+      <body className={inter.className}>
+        <ConfigureAmplifyClientSide />
+        {children}
+      </body>
     </html>
   );
 }
