1f0cb773d10c64b3e1aa3f2f90e00d8a80614189
GastonMora42 <gaston-mora@hotmail.com>
2025-01-22 17:10:40 -0300

    Agregamos favicon

--- src/pages/layout.tsx
+++ src/pages/layout.tsx
@@ -1,43 +1,28 @@
 // src/pages/layout.tsx
 import { inter } from "@/components/fonts";
 import ConfigureAmplifyClientSide from "@/amplify-cognito-config";
-import type { Metadata } from 'next';
-
-export const metadata: Metadata = {
- title: 'MB Admin',
- description: 'Sistema de Administración del Estudio de Danzas de Micaela Meindl',
- icons: {
-   icon: [
-     { url: '/favicon.ico' },
-     { url: '/icon-16.png', sizes: '16x16', type: 'image/png' },
-     { url: '/icon-32.png', sizes: '32x32', type: 'image/png' },
-     { url: '/icon-64.png', sizes: '64x64', type: 'image/png' }
-   ],
-   apple: [
-     { url: '/apple-icon.png', sizes: '180x180', type: 'image/png' }
-   ],
-   shortcut: ['/favicon.ico']
- }
-};
+import Head from 'next/head';
 
 export default function RootLayout({
- children,
+  children,
 }: {
- children: React.ReactNode;
+  children: React.ReactNode;
 }) {
- return (
-   <html lang="es">
-     <head>
-       <link rel="icon" href="/favicon.ico" />
-       <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon.png" />
-       <link rel="icon" type="image/png" sizes="32x32" href="/icon-32.png" />
-       <link rel="icon" type="image/png" sizes="16x16" href="/icon-16.png" />
-       <meta name="theme-color" content="#FFC001" />
-     </head>
-     <body className={inter.className}>
-       <ConfigureAmplifyClientSide />
-       {children}
-     </body>
-   </html>
- );
+  return (
+    <html lang="es">
+      <Head>
+        <title>MB Admin</title>
+        <meta name="description" content="Sistema de Administración del Estudio de Danzas de Micaela Meindl" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/icon-32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/icon-16.png" />
+        <meta name="theme-color" content="#FFC001" />
+      </Head>
+      <body className={inter.className}>
+        <ConfigureAmplifyClientSide />
+        {children}
+      </body>
+    </html>
+  );
 }
